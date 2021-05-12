c18fec7799afc8e97ea2c9a4d8b9a49440454c8e
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-05-12 10:44:44 +0200

    Add improvements js

--- main.js
+++ main.js
@@ -2,27 +2,9 @@
 
 const button = document.querySelector('.js-button');
 const inputNumber = document.querySelector('.js-num');
-const inputNumValue = inputNumber.value;
 const clueElement = document.querySelector('.js-clue');
-const clueContent = clueElement.innerHTML; 
+
 const inputCounter = document.querySelector('.js-counter');
-const inputCountValue = inputCounter.value;
-
-
-//genera comprobación juego
-function guessNumber() {
-    const toNum = parseInt(inputNumValue);
-    console.log(toNum);
-    if (toNum === getRandomNumber) {
-      clueContent = 'Has ganado campeona!';
-    } else if (toNum > getRandomNumber) {
-      clueContent = 'Demasiado alto.';
-    } else if (toNum < getRandomNumber) {
-      clueContent = 'Demasiado bajo.';
-    } else if (toNum < 1 || guessNumber > 100) {
-      clueContent = 'El número debe estar entre 1 y 100.';
-    }
-}
 
 //genera nº aleatorio
 function getRandomNumber(max) {
@@ -31,14 +13,37 @@ function getRandomNumber(max) {
 
 console.log(`EL número aleatorio es ${getRandomNumber(100)}`);
 
+const randomNumber = getRandomNumber(100);
+
+
+//genera comprobación juego
+function guessNumber() {
+    const inputNumValue = inputNumber.value;
+    const toNum = parseInt(inputNumValue);
+    console.log(toNum);
+
+
+    if (toNum === randomNumber) {
+        clueElement.innerHTML = '¡Has ganado campeona!';
+    } else if (toNum > randomNumber) {
+        clueElement.innerHTML = 'Demasiado alto';
+    } else if (toNum < randomNumber) {
+        clueElement.innerHTML = 'Demasiado bajo';
+    } else if (toNum < 1 || toNum > 100 ) {
+        clueElement.innerHTML = 'El número debe estar entre 1 y 100';
+    }
+}
+
+
+
 // función contador
 let attempts = 0;
 
 function countAttempts() {
     attempts = attempts + 1;
-    return inputCountValue = `Número de intentos: ${countAttempts()}`;
+    let inputCountValue = inputCounter.value;
+    inputCountValue = attempts; 
 }
-
 
 
 button.addEventListener('click', guessNumber);
